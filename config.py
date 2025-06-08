6b4667f753fe048814e89300b47fb3c52b898ad1
d6511 <d6511715@gmail.com>
2025-06-08 19:55:51 +0800

    01

--- config.py
+++ config.py
@@ -3,10 +3,8 @@ import os
 class Config:
     LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
     LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
-    SQLITE_DB_PATH = os.getenv('SQLITE_DB_PATH', 'data/lost_and_found.db') # 預設路徑
-
-    # 本地圖片儲存路徑
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
+    SQLITE_DB_PATH = '/tmp/lost_items.db' # 或 /tmp/lost_and_found.db
+    UPLOAD_FOLDER = '/tmp/uploads'
 
     HUGGINGFACE_API_URL = os.getenv('HUGGINGFACE_API_URL')
     HUGGINGFACE_API_TOKEN = os.getenv('HUGGINGFACE_API_TOKEN')
