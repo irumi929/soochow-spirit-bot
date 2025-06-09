6801f4b3c99f39911df13b850169f1d0bc1827b5
d6511 <d6511715@gmail.com>
2025-06-09 20:54:02 +0800

    lost-and-found-function

--- config.py
+++ config.py
@@ -10,8 +10,6 @@ class Config:
     LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
     LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
     SQLITE_DB_PATH = os.getenv('SQLITE_DB_PATH', 'data/lost_and_found.db') 
-
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
-
     HUGGINGFACE_API_URL = os.getenv('HUGGINGFACE_API_URL')
     HUGGINGFACE_API_TOKEN = os.getenv('HUGGINGFACE_API_TOKEN')
